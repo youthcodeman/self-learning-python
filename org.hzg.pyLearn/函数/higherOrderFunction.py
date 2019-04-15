1be21dc209e1c6734ea6629bc890a5c290f97158
youthcodeman <hzgaly@163.com>
2019-04-16 00:03:50 +0800

    [add] python内置函数和高阶函数

---
+++ org.hzg.pyLearn/函数/higherOrderFunction.py
@@ -0,0 +1,15 @@
+#_*_ coding:utf-8 _*_
+'''
+map
+'''
+# list01 = [1,2,3,4,5,6,7,8,9]
+# list02 = [1,2,3,4,5,6,7,8,9]
+# map = map(lambda x,y : x * y,list01,list02)
+# print(list(map))
+
+'''
+filter
+'''
+list01 = [1,2,3,4,5,6,7,8,9]
+filter = filter(lambda x : x > 5,list01)
+print(list(filter))
--- org.hzg.pyLearn/函数/lambdaFunction.py
+++ org.hzg.pyLearn/函数/lambdaFunction.py
@@ -8,5 +8,22 @@
 # m = lambda a,b : a*b
 # print(m(2000000,5000000))
 
-m = lambda a,b : a if a > b else b
-print(m(5,4))
+# m = lambda a,b : a if a > b else b
+# print(m(5,4))
+
+# dic = {"a":1,"c":3,"b":2}
+# #dic = sorted(dic)  #只是将字典中的key进行排序，并且返回排序后的key的列表
+# dic = sorted(dic.items(),reverse=True)
+# print({k:v for k,v in dic})
+
+# dic = {"a":1,"c":3,"b":2}
+# dic = sorted(dic.items(),key= lambda x:x[1],reverse=True) #字典需要转换成元组列表进行排序
+# print({k:v for k,v in dic})
+
+list01 = [
+    {"name":"zhangsan","age":18},
+    {"name":"lisi","age":20},
+    {"name":"wangwu","age":19}
+]
+dic = sorted(list01,key=lambda x:x['age'],reverse=True)
+print(dic)
